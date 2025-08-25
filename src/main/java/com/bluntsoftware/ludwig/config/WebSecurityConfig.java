e1320599ac0f803323438c918a9516dc69335701
bluntsoftware <alexm@bluntsoftware.com>
2025-08-25 13:31:58 -0400

    Update final spring boot 3.4.0 update

--- src/main/java/com/bluntsoftware/ludwig/config/WebSecurityConfig.java
+++ src/main/java/com/bluntsoftware/ludwig/config/WebSecurityConfig.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
